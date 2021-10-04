9c3c96f1cedf01e18221c421d795453cf51c6a4c
Bato007 <brandonhernandez_10@outlook.es>
2021-10-03 18:40:57 -0600

    space

---
+++ Assets/src/inversePlayerMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inversePlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vy = Input.GetAxis("Vertical") * moveSpeed;
+        float vx = Input.GetAxis("Horizontal") * moveSpeed;
+        if (rb)
+            rb.AddForce(new Vector2(-vy, vx));
+    }
+}
--- Assets/src/triggerHealth.cs
+++ Assets/src/triggerHealth.cs
@@ -1,14 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class triggerEvent : MonoBehaviour
+public class triggerHealth : MonoBehaviour
 {
     public GameObject option;
-    public bool allowed = false;
-    public int[] index;
-   
+
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (option)
@@ -17,15 +14,10 @@ public class triggerEvent : MonoBehaviour
         // Va para el Controll Room
         if (Input.GetKeyDown(KeyCode.R))
         {
-            SceneManager.LoadScene(index[0]);
-        }
-        // Va para hacer ejercicio
-        else if (Input.GetKeyDown(KeyCode.F) && allowed)
-        {
-            SceneManager.LoadScene(index[1]);
+            // Make something
         }
     }
-    
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (option)
